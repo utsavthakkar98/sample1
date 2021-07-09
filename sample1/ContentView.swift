a58ece2de9b820298b8db649762a15f726cfb169
Lucky <sowmya.muni@gmail.com>
2021-07-09 10:05:25 -0400

    Initial Commit

---
+++ sample1/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  sample1
+//
+//  Created by Lucky on 09/07/21.
+//  Copyright © 2021 Lucky. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ sample1Tests/sample1Tests.swift
@@ -0,0 +1,34 @@
+//
+//  sample1Tests.swift
+//  sample1Tests
+//
+//  Created by Lucky on 09/07/21.
+//  Copyright © 2021 Lucky. All rights reserved.
+//
+
+import XCTest
+@testable import sample1
+
+class sample1Tests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
